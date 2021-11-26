ad701050c5f6f0c726a64c7da6b3210adab29f5d
paulchicago74 <paulchicago74@gmail.com>
2021-11-25 19:39:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ report_text = st.text_input ("Here")
 
 report_text = st.text_input("Report Text")
 
-with st.expander("See explanation"):
+with st.expander("See explanation")
 st.write("""
 The chart above shows some numbers I picked for you.
 I rolled actual dice for these, so they're *guaranteed* to

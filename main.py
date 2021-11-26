38f3e400237432efdb585503cf65417b345a7e9a
paulchicago74 <paulchicago74@gmail.com>
2021-11-25 19:45:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ with st.expander("See explanation"):
     st.write("""
 The chart above shows some numbers I picked for you.
 I rolled actual dice for these, so they're *guaranteed* to
-be random."""):
+be random.""")
         st.image("https://static.streamlit.io/examples/dice.jpg")
 
 

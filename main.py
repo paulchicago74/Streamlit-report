260a139ebe14a3a06be94c9d1aeacba93fc1114e
paulchicago74 <paulchicago74@gmail.com>
2021-11-25 19:44:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ report_text = st.text_input("Report Text")
 with st.expander("See explanation"):st.write("""
 The chart above shows some numbers I picked for you.
 I rolled actual dice for these, so they're *guaranteed* to
-be random.""")
-st.image("https://static.streamlit.io/examples/dice.jpg")
+be random.""")st.image("https://static.streamlit.io/examples/dice.jpg")
 
 
 export_as_pdf = st.button("Export Report")

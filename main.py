b703d1c6a0929c2fa4c625dcd7e7e38b5f8c5cd1
paulchicago74 <paulchicago74@gmail.com>
2021-11-27 09:57:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -76,14 +76,14 @@ if submit:
     )
     
     
-   pop_data = ([
+   pop_data = [
         ['City', '2010 Population', '2000 Population'],
         ['New York City, NY', 8175000, 8008000],
         ['Los Angeles, CA', 3792000, 3694000],
         ['Chicago, IL', 2695000, 2896000],
         ['Houston, TX', 2099000, 1953000],
-        ['Philadelphia, PA', 1526000, 1517000],
-    ])
+        ['Philadelphia, PA', 1526000, 1517000]
+    ]
 
     gchart.gchart(key="city_chart", data=pop_data, chartType="BarChart", width='500px', height='300px', 
         title="Population of Largest U.S. Cities", hAxis={"title": 'Total Population', "minValue": 0}, vAxis={"title": 'City'} )

b66bf9f8b7ee8fdd12b6182bc73222a68996005d
paulchicago74 <paulchicago74@gmail.com>
2021-11-26 18:43:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,31 +23,6 @@ right.write("Here's the template we'll be using:")
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
 template = env.get_template("template.html")
 
-cat_data = [
-    ['Phrases'],
-    ['cats are better than dogs'],
-    ['cats eat kibble'],
-    ['cats are better than hamsters'],
-    ['cats are awesome'],
-    ['cats are people too'],
-    ['cats eat mice'],
-    ['cats meowing'],
-    ['cats in the cradle'],
-    ['cats eat mice'],
-    ['cats in the cradle lyrics'],
-    ['cats eat kibble'],
-    ['cats for adoption'],
-    ['cats are family'],
-    ['cats eat mice'],
-    ['cats are better than kittens'],
-    ['cats are evil'],
-    ['cats are weird'],
-    ['cats eat mice']
-]
-    
-gchart.gchart(key="cat_chart", data=cat_data, chartType="WordTree", 
-    width=600, height=400, wordtree={"format": "implicit", "word": "cats"}
-
 
 left.write("Fill in the data:")
 
@@ -101,5 +76,29 @@ if submit:
     )
     
     
+    cat_data = [
+    ['Phrases'],
+    ['cats are better than dogs'],
+    ['cats eat kibble'],
+    ['cats are better than hamsters'],
+    ['cats are awesome'],
+    ['cats are people too'],
+    ['cats eat mice'],
+    ['cats meowing'],
+    ['cats in the cradle'],
+    ['cats eat mice'],
+    ['cats in the cradle lyrics'],
+    ['cats eat kibble'],
+    ['cats for adoption'],
+    ['cats are family'],
+    ['cats eat mice'],
+    ['cats are better than kittens'],
+    ['cats are evil'],
+    ['cats are weird'],
+    ['cats eat mice']
+]
     
+gchart.gchart(key="cat_chart", data=cat_data, chartType="WordTree", 
+    width=600, height=400, wordtree={"format": "implicit", "word": "cats"}
+
 

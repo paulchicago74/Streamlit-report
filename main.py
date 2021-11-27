54cb2b7390b1c03ec144d1cd34bcd1a457952901
paulchicago74 <paulchicago74@gmail.com>
2021-11-26 18:49:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -76,26 +76,14 @@ if submit:
     )
     
     
-    cat_data = [
-    ['Phrases'],
-    ['cats are better than dogs'],
-    ['cats eat kibble'],
-    ['cats are better than hamsters'],
-    ['cats are awesome'],
-    ['cats are people too'],
-    ['cats eat mice'],
-    ['cats meowing'],
-    ['cats in the cradle'],
-    ['cats eat mice'],
-    ['cats in the cradle lyrics'],
-    ['cats eat kibble'],
-    ['cats for adoption'],
-    ['cats are family'],
-    ['cats eat mice'],
-    ['cats are better than kittens'],
-    ['cats are evil'],
-    ['cats are weird'],
-    ['cats eat mice']
-]
-    
-gchart.gchart(key="cat_chart", data=cat_data, chartType="WordTree", width=600, height=400, wordtree={"format": "implicit", "word": "cats"}
+   pop_data = [
+        ['City', '2010 Population', '2000 Population'],
+        ['New York City, NY', 8175000, 8008000],
+        ['Los Angeles, CA', 3792000, 3694000],
+        ['Chicago, IL', 2695000, 2896000],
+        ['Houston, TX', 2099000, 1953000],
+        ['Philadelphia, PA', 1526000, 1517000],
+    ]
+
+    gchart.gchart(key="city_chart", data=pop_data, chartType="BarChart", width='500px', height='300px', 
+        title="Population of Largest U.S. Cities", hAxis={"title": 'Total Population', "minValue": 0}, vAxis={"title": 'City'} )

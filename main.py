09c99dcc7ce7b753ae27b2cae73d0942dd78401c
paulchicago74 <paulchicago74@gmail.com>
2021-11-27 10:00:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -77,12 +77,12 @@ if submit:
     
     
    pop_data = ({
-        ['City', '2010 Population', '2000 Population'],
-        ['New York City, NY', 8175000, 8008000],
-        ['Los Angeles, CA', 3792000, 3694000],
-        ['Chicago, IL', 2695000, 2896000],
-        ['Houston, TX', 2099000, 1953000],
-        ['Philadelphia, PA', 1526000, 1517000]
+       ['City', '2010 Population', '2000 Population'],
+       ['New York City, NY', 8175000, 8008000],
+       ['Los Angeles, CA', 3792000, 3694000],
+       ['Chicago, IL', 2695000, 2896000],
+       ['Houston, TX', 2099000, 1953000],
+       ['Philadelphia, PA', 1526000, 1517000]
    })
 
     gchart.gchart(key="city_chart", data=pop_data, chartType="BarChart", width='500px', height='300px', 

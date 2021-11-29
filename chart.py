331a78b00da1b8774e7cca0de47149076d9ad276
paulchicago74 <paulchicago74@gmail.com>
2021-11-29 14:19:37 -0600

    Update chart.py

--- chart.py
+++ chart.py
@@ -67,7 +67,7 @@ chart = st.line_chart(df2)
 
 series = pd.DataFrame({
   'year': ['2010', '2011', '2012', '2013','2010', '2011', '2012', '2013'],
-  'animal': ['antelope', 'antelope', 'antelope', 'antelope', 'velociraptor', 'velociraptor', 'velociraptor', 'velociraptor',],
+ 
   'count': [8, 6, 3, 1, 2, 4, 5, 5]
 })
 
@@ -75,25 +75,15 @@ series = pd.DataFrame({
 basic_chart = alt.Chart(series).mark_line().encode(
     x='year',
     y='count',
-    color='animal',
+   
     # legend=alt.Legend(title='Animals by year')
-)
-
-# Custom Altair line chart where you set color and specify dimensions
-custom_chart = alt.Chart(series).mark_line().encode(
-    x='year',
-    y='count',
-    color=alt.Color('animal',
-            scale=alt.Scale(
-                domain=['antelope', 'velociraptor'],
-                range=['blue', 'red'])
-                )
-).properties(
+    
+    ).properties(
     width=900,
     height=500
 )
 
 st.altair_chart(basic_chart)
-st.altair_chart(custom_chart)
+
 
                 

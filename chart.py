8cfde3841c3f385ca898cdc4dc038de95e50ad5c
paulchicago74 <paulchicago74@gmail.com>
2021-11-29 12:23:52 -0600

    Update chart.py

--- chart.py
+++ chart.py
@@ -86,7 +86,7 @@ df = pd.DataFrame(
 np.random.randn(200, 3),
 columns=['a', 'b', 'c'])
 
-c = alt.Chart(df).mark_circle().encode(
-x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_line().encode(
+x='a', y='b', size='c', color='c', tooltip=['Dvalue'])
     
 st.altair_chart(c, use_container_width=True)

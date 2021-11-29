37f14a2a7434b668850278e0b52f0791205f03ba
paulchicago74 <paulchicago74@gmail.com>
2021-11-29 12:13:26 -0600

    Update chart.py

--- chart.py
+++ chart.py
@@ -69,3 +69,16 @@ alt.Chart(df2).mark_line().encode(
     
 )
   
+x = np.arange(100)
+source = pd.DataFrame({
+  'x': x,
+  'f(x)': np.sin(x / 5)
+})
+
+alt.Chart(source).mark_line(
+    point=alt.OverlayMarkDef(color="red")
+).encode(
+    x='x',
+    y='f(x)'
+)
+    

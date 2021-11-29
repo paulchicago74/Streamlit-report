b515d0b64ab2be6c385e3d8c2bebce107a1a4710
paulchicago74 <paulchicago74@gmail.com>
2021-11-29 14:12:00 -0600

    Update chart.py

--- chart.py
+++ chart.py
@@ -68,7 +68,7 @@ df2
 
 df2 = pd.DataFrame([Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10]
 )
-fig = px.line(df2, x="year", y="lifeExp", title='Life expectancy in Canada')
+fig = px.line(df2, x="D-value", y="lifeExp", title='Life expectancy in Canada')
 
 st.plotly_chart(fig, use_container_width=True)
 

92e17320c9c50174e65cbc7f12e1803771e9acba
paulchicago74 <paulchicago74@gmail.com>
2021-11-29 14:01:40 -0600

    Update chart.py

--- chart.py
+++ chart.py
@@ -71,8 +71,8 @@ columns=['a'])
 st.vega_lite_chart(df3, {
 'mark': {'type': 'line', 'tooltip': True},
  'encoding': {
-'x': {'field': 'a', 'type': 'quantitative'},
-'y': {'field': 'b', 'type': 'quantitative'},
+
+'y': {'field': 'a', 'type': 'quantitative'},
 'size': {'field': 'c', 'type': 'quantitative'},
 'color': {'field': 'c', 'type': 'quantitative'},
 },

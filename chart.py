0d76d59c948e2405e5f1784f414a9044509ca786
paulchicago74 <paulchicago74@gmail.com>
2021-11-29 13:43:15 -0600

    Update chart.py

--- chart.py
+++ chart.py
@@ -66,7 +66,7 @@ df2
 
 alt.Chart(pd.DataFrame({
     #'F-value': [Fvalue0, Fvalue1, Fvalue2, Fvalue3, Fvalue4, Fvalue5, Fvalue6, Fvalue7, Fvalue8, Fvalue9, Fvalue10],
-    'D-value': [Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10]
+   [Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10]
 }).mark_line()
                 
 #st.altair_chart(df2)

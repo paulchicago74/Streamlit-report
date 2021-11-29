0166f6ead39308bcfe9b9d10d850f7ebc3ef930f
paulchicago74 <paulchicago74@gmail.com>
2021-11-29 11:31:37 -0600

    Update chart.py

--- chart.py
+++ chart.py
@@ -16,7 +16,7 @@ Time = 25
 Dvalue = 10
 
 Fvalue0 = 0
-Fvalue1 = (Fvalue0 + (10 ** ((Temp - Tref)/Zref)) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/10) - 0 ))
+Fvalue1 = (Fvalue0 + (10 ** ((Temp - Tref)/Zref)) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/10) - 0))
 Fvalue2 = (Fvalue1 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/9) - (Time/10)))
 Fvalue3 = (Fvalue1 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time) - (Time/9)))
 #D= Fvalue / Dvalue

1c46f6f75c64828d6ba317ae168181ef309cc72c
paulchicago74 <paulchicago74@gmail.com>
2021-11-30 09:51:25 -0600

    Update chart.py

--- chart.py
+++ chart.py
@@ -76,7 +76,7 @@ series = pd.DataFrame({
 })
 
 y = np.array([Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10])
-x = np.array([0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1]).reshape((-1, 1))
+x = np.array([0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1])
 #x = [Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10]
 #y = [0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1]
 #x, y = np.array(x), np.array(y)

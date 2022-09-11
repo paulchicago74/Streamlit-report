5c22324585d30f66f9af4abe31d5e37b38d3507b
paulchicago74 <paulchicago74@gmail.com>
2022-09-11 10:33:46 -0500

    Update chart.py

--- chart.py
+++ chart.py
@@ -1,123 +1,60 @@
-import pdfkit
-from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
-from datetime import date
 import streamlit as st
-from streamlit.components.v1 import iframe
 import pandas as pd
-import numpy as np
-import streamlit_gchart as gchart
-import plotly.offline as offline
-import plotly.graph_objs as go
-import altair as alt
-import plotly.figure_factory as ff
-import plotly.express as px
-from sklearn.linear_model import LinearRegression
-from statsmodels.formula.api import ols
-from sklearn.metrics import mean_squared_error, r2_score
-import statsmodels.api as sm
-from millify import millify
-from streamlit_option_menu import option_menu
-#import pyscreenshot
-#import pyautogui
+import plotly.graph_objects as go
+import io
 
+# Load the data
+@st.experimental_memo
+def load_data():
+    return pd.DataFrame(
+        {
+            "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+            "Contestant": ["Alex", "Alex", "Alex", "Jordan", "Jordan", "Jordan"],
+            "Number Eaten": [2, 1, 3, 1, 3, 2],
+        }
+    )
 
-Temp = 150
-Tref = 144
-Zref = 10
-Time = 25
+# Create and cache a Plotly figure
+@st.experimental_memo
+def create_figure(df):
+    fig = go.Figure()
+    for contestant, group in df.groupby("Contestant"):
+        fig.add_trace(
+            go.Bar(
+                x=group["Fruit"],
+                y=group["Number Eaten"],
+                name=contestant,
+                hovertemplate="Contestant=%s<br>Fruit=%%{x}<br>Number Eaten=%%{y}<extra></extra>"
+                % contestant,
+            )
+        )
+    fig.update_layout(legend_title_text="Contestant")
+    fig.update_xaxes(title_text="Fruit")
+    fig.update_yaxes(title_text="Number Eaten")
+    return fig
 
-Wanted_D = st.sidebar.number_input('Enter the Dvalue', min_value=0.1, step=0.1)
-Dvalue = st.sidebar.number_input('Enter the reference Dvalue', min_value=0.1, step=0.1)
+df = load_data()
+fig = create_figure(df)
 
-Fvalue0 = 0
-Fvalue1 = (Fvalue0 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/10) - 0))
-Fvalue2 = (Fvalue1 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/9) - (Time/10)))
-Fvalue3 = (Fvalue2 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/8) - (Time/9)))
-Fvalue4 = (Fvalue3 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/7) - (Time/8)))
-Fvalue5 = (Fvalue4 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/6) - (Time/7)))
-Fvalue6 = (Fvalue5 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/5) - (Time/6)))
-Fvalue7 = (Fvalue6 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/4) - (Time/5)))
-Fvalue8 = (Fvalue7 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/3) - (Time/4)))
-Fvalue9 = (Fvalue8 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/2) - (Time/3)))
-Fvalue10 = (Fvalue9 + (10 ** ((Temp - Tref)/Zref) + (10 ** ((Temp - Tref)/Zref)))/2*((Time/1) - (Time/2)))
+# Create an in-memory buffer
+buffer = io.BytesIO()
 
-Dvalue0 = 0
-Dvalue1 = Fvalue1 / Dvalue
-Dvalue2 = Fvalue2 / Dvalue
-Dvalue3 = Fvalue3 / Dvalue
-Dvalue4 = Fvalue4 / Dvalue
-Dvalue5 = Fvalue5 / Dvalue
-Dvalue6 = Fvalue6 / Dvalue
-Dvalue7 = Fvalue7 / Dvalue
-Dvalue8 = Fvalue8 / Dvalue
-Dvalue9 = Fvalue9 / Dvalue
-Dvalue10 = Fvalue10 / Dvalue
+# Save the figure as a pdf to the buffer
+fig.write_image(file=buffer, format="pdf")
 
-#D= Fvalue / Dvalue
+# Download the pdf from the buffer
+st.download_button(
+    label="Download PDF",
+    data=buffer,
+    file_name="figure.pdf",
+    mime="application/pdf",
+)
 
-metric = 100 / 10
-
-# sample data
-df = pd.DataFrame({
-    'F-value': [Fvalue0, Fvalue1, Fvalue2, Fvalue3, Fvalue4, Fvalue5, Fvalue6, Fvalue7, Fvalue8, Fvalue9, Fvalue10],
-    #'D-value': [Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10]
-})
-
-#df
-
-df2 = pd.DataFrame({
-    #'F-value': [Fvalue0, Fvalue1, Fvalue2, Fvalue3, Fvalue4, Fvalue5, Fvalue6, Fvalue7, Fvalue8, Fvalue9, Fvalue10],
-    'Dvalue': [Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10],
-    #'Time': [0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1],
-    
-})
-
-df2
-
-chart = st.line_chart(df)
-chart = st.line_chart(df2)
-st.plotly_chart(df)
-
-y = np.array([Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10]).reshape((-1, 1))
-#x = np.array([0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1]).reshape((-1, 1))
-x_1 = np.array([Dvalue0, Dvalue1, Dvalue2, Dvalue3, Dvalue4, Dvalue5, Dvalue6, Dvalue7, Dvalue8, Dvalue9, Dvalue10])
-y_1 = np.array([0, Time/10, Time/9, Time/8, Time/7, Time/6, Time/5, Time/4, Time/3, Time/2, Time/1])
-#x, y = np.array(x), np.array(y)
-#model = LinearRegression()
-#model.fit(y, x)
-n = np.size(x_1)
-n_xy = x_1 * y_1
-x_mean = np.mean(x_1)
-y_mean = np.mean(y_1)
-Sxy = np.sum(x_1*y_1)- n*x_mean*y_mean
-Sxx = np.sum(x_1*x_1) - n*x_mean*x_mean
-b1 = Sxy/Sxx
-b0 = y_mean-b1*x_mean
-#st.write('slope b1 is', b1)
-#st.write('intercept b0 is', b0)
-st.write('The time you need for a D-value of', Wanted_D ,'is' , millify(b1*Wanted_D + b0, precision=2))
+st.plotly_chart(fig)
 
 
 
-with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
-   
            
         
-#image = pyscreenshot.grab()
-#image.show()
-  
-# To save the screenshot
-#image.save("GeeksforGeeks.png")
-screenshot = pyautogui.screenshot()
-screenshot.save("screen.png")
-with open("screen.png", "rb") as file:
-        btn = st.download_button(
-             label="Download image",
-             data=file,
-             file_name="screen.png",
-             mime="image/png"
-           )
+
 
